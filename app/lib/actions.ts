aad78bcdcfd9187f47603d21e495669139d4570d
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-12-29 18:06:19 +0300

    Add actions.ts file with createCourse and getCoursesByDay functions

---
+++ app/lib/actions.ts
@@ -0,0 +1,63 @@
+"use server";
+
+import prisma from "@/app/lib/prisma";
+import { Course } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
+type Result<T> = { success: true; data: T } | { success: false; error: Error };
+
+/**
+ * Creates a new course in the database.
+ *
+ * @param currentState Current state of the form.
+ * @param formData Submitted form data.
+ * @returns Error message if any.
+ */
+export async function createCourse(
+  currentState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    const { name, day, time, grade, classNumber, instructor } =
+      Object.fromEntries(formData.entries());
+
+    await prisma.course.create({
+      data: {
+        name: String(name),
+        day: String(day),
+        time: String(time),
+        grade: String(grade),
+        classNumber: Number(classNumber),
+        instructor: {
+          connect: {
+            id: Number(instructor),
+          },
+        },
+      },
+    });
+
+    revalidatePath("/");
+  } catch (error) {
+    return `Error while creating course: ${error}`;
+  }
+}
+
+/**
+ * Filters courses by given day.
+ *
+ * @param day Day to filter by.
+ * @returns Filtered courses or error.
+ */
+export async function getCoursesByDay(day: string): Promise<Result<Course[]>> {
+  try {
+    const courses = await prisma.course.findMany({
+      where: {
+        day: day,
+      },
+    });
+
+    return { success: true, data: courses };
+  } catch (error) {
+    return { success: false, error: error as Error };
+  }
+}
