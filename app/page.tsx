4d8b5684baa6ac4f4af6cc18df9e71adfed941cc
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-12-29 18:05:33 +0300

    Add CourseForm and Schedule components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import CourseForm from "./components/courseForm";
+import Schedule from "./components/schedule";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <CourseForm />
+      <Schedule />
+    </main>
   );
 }
