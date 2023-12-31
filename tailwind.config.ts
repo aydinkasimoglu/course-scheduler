c796dfe9670666ee2364aaf458ae143e027d5666
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2023-12-31 17:44:55 +0300

    Update tailwind.config.ts to include @tailwindcss/forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/forms")],
+};
+export default config;
