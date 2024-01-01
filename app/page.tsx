f2c57cf43c209a08eeff98b82360da45e0f651c9
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-01-01 21:27:50 +0300

    Update imports in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import CourseForm from "./components/courseForm";
-import Schedule from "./components/schedule";
+import CourseForm from "@/app/components/courseForm";
+import InstructorForm from "@/app/components/instructorForm";
+import Schedule from "@/app/components/schedule";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CourseForm />
+      <InstructorForm />
       <Schedule />
     </main>
   );
