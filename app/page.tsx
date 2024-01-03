3f7522c24e97fd1ad8e3e65cc20c998a93476de6
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-01-03 18:25:53 +0300

    Add InstructorDelete component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import CourseForm from "@/app/components/courseForm";
 import InstructorForm from "@/app/components/instructorForm";
 import Schedule from "@/app/components/schedule";
+import InstructorDelete from "@/app/components/instructorDelete";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <CourseForm />
-      <InstructorForm />
+    <main className="flex min-h-screen flex-col justify-between p-12">
+      <div className="mb-4 flex w-full flex-col justify-end gap-3 md:flex-row">
+        <CourseForm />
+        <InstructorForm />
+        <InstructorDelete />
+      </div>
       <Schedule />
     </main>
   );
