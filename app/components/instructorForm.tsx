c099c509918f30c87bf4815cd303ef3ea799771d
aydinkasimoglu <aydinkasimoglu.03@gmail.com>
2024-01-05 16:21:29 +0300

    Refactor instructor form component

--- app/components/instructorForm.tsx
+++ app/components/instructorForm.tsx
@@ -3,23 +3,25 @@
 import { useEffect, useRef } from "react";
 import { useFormState } from "react-dom";
 import { createInstructor } from "@/app/lib/actions";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { useInstructors } from "@/app/components/instructorProvider";
 
 export default function InstructorForm() {
   const dialogRef = useRef<HTMLDialogElement>(null);
-  const nameRef = useRef<HTMLInputElement>(null);
   const [errorMessage, dispatch] = useFormState(createInstructor, undefined);
+  const { loadInstructors } = useInstructors();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<{ name: string }>();
 
   useEffect(() => {
     if (errorMessage) {
       alert(errorMessage);
     }
   }, [errorMessage]);
-
-  function resetForm() {
-    if (nameRef.current) {
-      nameRef.current.value = "";
-    }
-  }
 
   function closeDialog() {
     if (dialogRef.current) {
@@ -28,12 +30,24 @@ export default function InstructorForm() {
   }
 
   function openDialog() {
-    resetForm();
+    reset();
 
     if (dialogRef.current) {
       dialogRef.current.showModal();
     }
   }
+
+  const onSubmit: SubmitHandler<{ name: string }> = (data) => {
+    const formData = new FormData();
+
+    formData.append("name", data.name);
+
+    dispatch(formData);
+
+    closeDialog();
+
+    loadInstructors();
+  };
 
   return (
     <>
@@ -46,22 +60,26 @@ export default function InstructorForm() {
       </button>
       <dialog className="absolute m-auto bg-transparent" ref={dialogRef}>
         <form
+          onSubmit={handleSubmit(onSubmit)}
           className="mb-4 rounded-md px-8 pb-8 pt-6 shadow-md"
-          action={dispatch}
         >
           <div className="mb-4">
             <label className="styledLabel" htmlFor="name">
               Name
             </label>
             <input
-              ref={nameRef}
               id="name"
               className="textfield"
-              name="name"
               type="text"
               placeholder="Name"
-              required
+              {...register("name", { required: true })}
             />
+
+            {errors.name && errors.name.type === "required" && (
+              <span className="text-xs italic text-red-500" role="alert">
+                This field is required
+              </span>
+            )}
           </div>
 
           <div className="mt-6 flex items-center justify-end gap-x-6">
@@ -74,7 +92,6 @@ export default function InstructorForm() {
             </button>
             <button
               type="submit"
-              onClick={closeDialog}
               className="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
               Add
